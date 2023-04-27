312653f280a31ad9c82efad24cc10c4a33047466
Snezhana-star <cnega.2002@gmail.com>
2023-04-27 15:25:42 +0700

    proba

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,57 +1,22 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Pop it MVC</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>У-МУ</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 <header>
     <nav>
-        <a href="<?= app()->route->getUrl('/hello') ?>">Главная</a>
-        <?php
-        if (!app()->auth::check()):
-            ?>
-            <a href="<?= app()->route->getUrl('/login') ?>">Вход</a>
-            <a href="<?= app()->route->getUrl('/signup') ?>">Регистрация</a>
-        <?php
-        else:
-            ?>
-            <a href="<?= app()->route->getUrl('/logout') ?>">Выход (<?= app()->auth::user()->name ?>)</a>
-        <?php
-        endif;
-        ?>
+        <h1 class="Logo">У-МУ</h1>
+        <div></div>
     </nav>
+
 </header>
 <main>
     <?= $content ?? '' ?>
 </main>
-
 </body>
 </html>
-
-
-<!--<!DOCTYPE html>-->
-<!--<html lang="en">-->
-<!--<head>-->
-<!--    <meta charset="UTF-8">-->
-<!--    <meta http-equiv="X-UA-Compatible" content="IE=edge">-->
-<!--    <meta name="viewport" content="width=device-width, initial-scale=1.0">-->
-<!--    <title>У-МУ</title>-->
-<!--    <link href="css/style.css" rel="stylesheet" type="text/css">-->
-<!--</head>-->
-<!--<body>-->
-<!--<header>-->
-<!--    <nav>-->
-<!--        <h1 class="Logo">У-МУ</h1>-->
-<!--        <div></div>-->
-<!--    </nav>-->
-<!---->
-<!--</header>-->
-<!--<main>-->
-<!--    --><?php //= $content ?? '' ?>
-<!--</main>-->
-<!--</body>-->
-<!--</html>-->
