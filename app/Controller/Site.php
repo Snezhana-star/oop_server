1c882c139948aef60ede60707048d032b023ca7c
Snezhana-star <cnega.2002@gmail.com>
2023-04-30 15:48:35 +0700

    3 task

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -5,6 +5,8 @@ use Model\Post;
 use Src\View;
 use Model\User;
 use Src\Auth\Auth;
+use Src\Validator\Validator;
+
 
 class Site
 {
@@ -20,11 +22,29 @@ class Site
     }
     public function signup(Request $request): string
     {
-        if ($request->method==='POST' && User::create($request->all())){
-            app()->route->redirect('/hello');
+        if ($request->method === 'POST') {
+
+            $validator = new Validator($request->all(), [
+                'name' => ['required'],
+                'login' => ['required', 'unique:users,login'],
+                'password' => ['required']
+            ], [
+                'required' => 'Поле :field пусто',
+                'unique' => 'Поле :field должно быть уникально'
+            ]);
+
+            if($validator->fails()){
+                return new View('site.signup',
+                    ['message' => json_encode($validator->errors(), JSON_UNESCAPED_UNICODE)]);
+            }
+
+            if (User::create($request->all())) {
+                app()->route->redirect('/login');
+            }
         }
         return new View('site.signup');
     }
+
     public function login(Request $request): string
     {
         //Если просто обращение к странице, то отобразить форму
